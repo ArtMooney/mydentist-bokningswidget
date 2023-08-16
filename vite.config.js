1624f5707a165476c45ca73be65bb04951c800cb
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-08-16 14:45:31 +0200

    more page router-testing

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy({
-      // Configuring the legacy plugin
-      targets: ["defaults", "not IE 11"],
-    }),
-  ],
-  server: {
-    // Configuring the server
-    rewrite: {
-      // Rewrite rule for handling URL routing
-      "^/boka/.*$": "/index.html",
-    },
-  },
+  plugins: [vue()],
   build: {
     assetsInlineLimit: 51200,
   },

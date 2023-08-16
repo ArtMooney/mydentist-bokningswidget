ccc885074d1fb23e833c7ca01989a400218c7522
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-08-16 14:29:42 +0200

    testing the fix but in vitejs instead

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    legacy({
+      // Configuring the legacy plugin
+      targets: ["defaults", "not IE 11"],
+    }),
+  ],
+  server: {
+    // Configuring the server
+    rewrite: {
+      // Rewrite rule for handling URL routing
+      "^/boka/.*$": "/index.html",
+    },
+  },
   build: {
     assetsInlineLimit: 51200,
   },

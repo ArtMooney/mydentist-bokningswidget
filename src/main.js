a7976fac51c1edbcfe2e97bf9dcec2c1126c746b
Henrik Skoglund <henrik.skoglund@framecore.se>
2023-09-08 16:50:28 +0200

    added font awesome

--- src/main.js
+++ src/main.js
@@ -6,16 +6,14 @@ import "./css/normalize.css";
 import "./css/components.css";
 import "./css/mydentist-apps.css";
 
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-// import { faXmark } from "@fortawesome/free-solid-svg-icons";
-// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-// library.add(faXmark, faMagnifyingGlass);
+library.add(faXmark, faMagnifyingGlass);
 
-// createApp(App)
-//   .use(createRouter())
-//   .component("font-awesome-icon", FontAwesomeIcon)
-//   .mount("#app");
-
-createApp(App).use(createRouter()).mount("#app");
+createApp(App)
+  .use(createRouter())
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
